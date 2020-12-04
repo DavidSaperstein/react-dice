36949f75116940cf6e106214d6370b2bc7367fe2
DavidSaperstein <d.h.saperstein@gmail.com>
2020-12-04 16:10:34 -0700

    deleted parent remnants

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from "react"
 import DiceBox from './DiceBox'
-import Parent from './Parent'
 import "./styles.css" 
 
 export default function App() {
@@ -10,10 +9,6 @@ export default function App() {
             <h1>Roll the Dice!</h1>
             <DiceBox />
         </div>
-            {/* <Parent text="text">
-                <h1>{props.text}</h1>
-                <h1></h1>
-            </Parent> */}
         </>
     )
 }
