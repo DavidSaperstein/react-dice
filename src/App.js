fbf8f4ff7e37226d84b877dafcc058f7bb2b36dc
DavidSaperstein <d.h.saperstein@gmail.com>
2020-12-04 17:37:23 -0700

    added suggestion to select dice"

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ export default function App() {
         <>
         <div className='dicebox-container'>
             <h1>Roll the Dice!</h1>
+            <h3>(Select the dice you don't want to roll)</h3>
             <DiceBox />
         </div>
         </>
